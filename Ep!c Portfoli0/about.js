714b877206b4e7da129c41144412f138cf571092
Anvarbek-011 <anvarmurodov200@gmail.com>
2025-09-16 19:37:07 +0500

    Add files via upload

---
+++ Ep!c Portfoli0/about.js
@@ -0,0 +1,5 @@
+const ElBtn = document.querySelector('.header-logo');
+
+ElBtn.addEventListener('click', function(){
+    document.body.classList.toggle('dark_mode')
+})
---
+++ Ep!c Portfoli0/script.js
@@ -0,0 +1,12 @@
+let ism = prompt ("Ismingizni kiriting:");
+let fam = prompt ("Familiyangizni kiritng:");
+let yosh = prompt ("Yoshingizni kiriting:");
+
+alert("Ism: " + ism + "Familiya:" + fam + " Yosh: " + yosh)
+console.log("prompt + ism + fam + yosh")
+
+let gmai = prompt ("Gmail");
+let addr = prompt ("Address");
+
+alert("Gmail:" + gmai + " Address: " + addr)
+console.log("prompt + ism + fam + yosh")
